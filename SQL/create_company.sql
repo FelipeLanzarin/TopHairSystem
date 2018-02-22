e3a85d380b1fbeb36efe97da66dd728a8ce6fa38
FelipeLanzarin <lanzarin.felipe@gmail.com>
2018-02-22 14:14:52 -0300

    sql

--- SQL/create_company.sql
+++ SQL/create_company.sql
@@ -1,2 +1,4 @@
 ﻿INSERT INTO company(id, creation_date,name) VALUES (1, now(), 'Mari Ruas Cabeleireira');
 INSERT INTO branch_company( id, creation_date, description, company_id)   VALUES (1, now(), 'Unidade de Lagoa Vermelha', 1);
+INSERT INTO cashier(id, balance, description, branch_company_id)   VALUES (1, 0, 'Caixa matriz', 1);
+
